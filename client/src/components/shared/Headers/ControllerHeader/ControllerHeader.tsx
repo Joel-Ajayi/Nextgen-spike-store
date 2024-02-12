e474ac0d1b3f0297c2f8e720025aa5c4c0993773
Joel-Ajayi <ajayiayotunde13@gmail.com>
2024-02-12 17:50:24 +0100

    Adjustment to click and highlight indicator of tabs in controller header and sidebar

--- client/src/components/shared/Headers/ControllerHeader/ControllerHeader.tsx
+++ client/src/components/shared/Headers/ControllerHeader/ControllerHeader.tsx
@@ -4,16 +4,21 @@ import { Roles } from "../../../../types";
 import UserAvatar from "../UserAvatar/UserAvatar";
 import Styles from "./styles.module.scss";
 import Dropdown from "../../Dropdown/Dropdown";
-import { Link, useLocation, useNavigate, useSearchParams } from "react-router-dom";
+import {
+  Link,
+  useLocation,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
 import userSlice from "../../../../store/userState";
 import { Pages } from "../../../../types/controller";
 import userReq from "../../../../requests/user";
 import ControllerSideBar from "./ControllerSideBar/ControllerSideBar";
 import { loginDropdown } from "../AppHeader/Header";
-import uniqId from 'uniqid'
+import uniqId from "uniqid";
 import appSlice from "../../../../store/appState";
 import controllerStateSlice from "../../../../store/controller/states";
-import navData, { DataType } from './data'
+import navData, { DataType } from "./data";
 import data from "./data";
 
 function ControllerHeader() {
@@ -28,24 +33,26 @@ function ControllerHeader() {
   const tab = params.get("sub");
 
   const { role, isAuthenticated } = useAppSelector((state) => state.user);
-  const activeTabs = useAppSelector((state) => state.controller.state.activeTabs);
+  const activeTabs = useAppSelector(
+    (state) => state.controller.state.activeTabs
+  );
 
   useEffect(() => {
     if (tab && !activeTabs.includes(tab)) {
       const tabPath: string[] = [];
 
-      (function findPath(data: { [x: string]: DataType; }, path: string[]) {
+      (function findPath(data: { [x: string]: DataType }, path: string[]) {
         Object.keys(data).forEach((dataKey) => {
           if (dataKey !== tab) {
-            findPath(data[dataKey].items, [...path, dataKey])
+            findPath(data[dataKey].items, [...path, dataKey]);
             return;
           }
-          tabPath.push(...path, dataKey)
-        })
-      })(data, [])
-      dispatch(setActiveTabs(tabPath))
+          tabPath.push(...path, dataKey);
+        });
+      })(data, []);
+      dispatch(setActiveTabs(tabPath));
     }
-  }, [tab])
+  }, [tab]);
 
   const handleSignOut = async () => {
     await userReq.signOut();
@@ -87,26 +94,35 @@ function ControllerHeader() {
           </Link>
         </div>
 
-        {role === Roles.SuperAdmin && Object.values(Pages).map((page) => {
-          const hasItems = !!Object.keys(navData[page]).length;
-          return <Dropdown
-            wrapperClassName={Styles.nav_item}
-            key={uniqId()}
-            listClassName={Styles.nav_item_dropdown}
-            title={navData[page].title}
-            showCaret
-            onClick={hasItems ? () => { } : null as any}
-            link={!hasItems ? navData[page].link : ''}
-            showToolTip={false}
-            items={Object.values(navData[page].items)}
-            level={1}
-          />
-        })}
+        {role === Roles.SuperAdmin &&
+          Object.values(Pages).map((page) => {
+            const hasItems = !!Object.keys(navData[page]).length;
+            return (
+              <Dropdown
+                wrapperClassName={Styles.nav_item}
+                key={uniqId()}
+                listClassName={Styles.nav_item_dropdown}
+                title={navData[page].title}
+                showCaret
+                onClick={hasItems ? () => {} : (null as any)}
+                link={!hasItems ? navData[page].link() : ""}
+                showToolTip={false}
+                items={Object.values(navData[page].items)}
+                level={1}
+              />
+            );
+          })}
 
         <Dropdown
           wrapperClassName={Styles.nav_item}
           onClick={handleSignInButton}
-          title={isAuthenticated ? <UserAvatar infoClassName={Styles.nav_avatar} /> : <span>Login</span>}
+          title={
+            isAuthenticated ? (
+              <UserAvatar infoClassName={Styles.nav_avatar} />
+            ) : (
+              <span>Login</span>
+            )
+          }
           listClassName={Styles.user_dropdown_list}
           titleClassName={!isAuthenticated ? Styles.login_button : ""}
           showCaret={false}
--- client/src/components/shared/Headers/ControllerHeader/ControllerSideBar/ControllerSideBar.tsx
+++ client/src/components/shared/Headers/ControllerHeader/ControllerSideBar/ControllerSideBar.tsx
@@ -1,67 +1,76 @@
-import React, { useEffect, useRef } from 'react'
-import Styles from './sidebar.module.scss';
-import { HiBars3BottomLeft as SideBarIcon } from 'react-icons/hi2'
-import { CONSTS } from '../../../../../const';
-import TabSideBar from './TabSideBar/TabSideBar';
+import React, { useEffect, useRef } from "react";
+import Styles from "./sidebar.module.scss";
+import { HiBars3BottomLeft as SideBarIcon } from "react-icons/hi2";
+import { CONSTS } from "../../../../../const";
+import TabSideBar from "./TabSideBar/TabSideBar";
 import controllerStateSlice from "../../../../../store/controller/states";
-import { useAppDispatch, useAppSelector } from '../../../../../store/hooks';
+import { useAppDispatch, useAppSelector } from "../../../../../store/hooks";
 
 type SideBarProp = {
-    isFixed: boolean;
-}
+  isFixed: boolean;
+};
 
 function ControllerSideBar({ isFixed }: SideBarProp) {
-    const dispatch = useAppDispatch();
-    const { showSideBar: showBar, sideBarWrapperStyle: wrapperStyle, sideBarStyle: style } = useAppSelector((state) => state.controller.state);
+  const dispatch = useAppDispatch();
+  const {
+    showSideBar: showBar,
+    sideBarWrapperStyle: wrapperStyle,
+    sideBarStyle: style,
+  } = useAppSelector((state) => state.controller.state);
 
-    const wrapperRef = useRef<HTMLDivElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
 
-    const { setSideBarState, setSideBarStyle, setSideBarWrapperStyle } = controllerStateSlice.actions;
+  const { setSideBarState, setSideBarStyle, setSideBarWrapperStyle } =
+    controllerStateSlice.actions;
 
-    const handleToggle = (e: MouseEvent) => {
-        if (showBar) {
-            const paths = e.composedPath()
-            const show = paths.findIndex(
-                (el) => (el as any).id === CONSTS.ids.appSideBar
-            ) !== -1;
-            if (!show) {
-                dispatch(setSideBarState(show));
-                dispatch(setSideBarWrapperStyle({ backgroundColor: "transparent", left: 0 }));
-                dispatch(setSideBarStyle({ left: "-100%" }));
-                setTimeout(() => {
-                    dispatch(setSideBarWrapperStyle({ left: "-100%" }));
-                }, 200);
-            }
-        }
-    };
-
-    const handleBarAction = () => {
-        dispatch(setSideBarWrapperStyle({ left: 0 }));
-        dispatch(setSideBarStyle({ left: 0 }));
-        dispatch(setSideBarState(true));
+  const handleToggle = (e?: MouseEvent) => {
+    if (showBar && e) {
+      const paths = e.composedPath();
+      const show =
+        paths.findIndex((el) => (el as any).id === CONSTS.ids.appSideBar) !==
+        -1;
+      if (!show) {
+        dispatch(setSideBarState(show));
+        dispatch(
+          setSideBarWrapperStyle({ backgroundColor: "transparent", left: 0 })
+        );
+        dispatch(setSideBarStyle({ left: "-100%" }));
+        setTimeout(() => {
+          dispatch(setSideBarWrapperStyle({ left: "-100%" }));
+        }, 200);
+      }
+    } else {
+      dispatch(setSideBarWrapperStyle({ left: 0 }));
+      dispatch(setSideBarStyle({ left: 0 }));
+      dispatch(setSideBarState(true));
     }
+  };
 
-    useEffect(() => {
-        if (wrapperRef.current && isFixed) {
-            wrapperRef.current.addEventListener("click", handleToggle);
-        }
-        return () => {
-            if (wrapperRef.current) {
-                wrapperRef.current.removeEventListener("click", handleToggle);
-            }
-        };
-    }, [wrapperRef.current, style]);
+  useEffect(() => {
+    if (wrapperRef.current && isFixed) {
+      wrapperRef.current.addEventListener("click", handleToggle);
+    }
+    return () => {
+      if (wrapperRef.current) {
+        wrapperRef.current.removeEventListener("click", handleToggle);
+      }
+    };
+  }, [wrapperRef.current, style]);
 
-    return (
-        <div className={Styles.bar_wrapper}>
-            <SideBarIcon className={Styles.icon} onClick={handleBarAction} />
-            <div style={wrapperStyle} ref={wrapperRef} className={isFixed ? Styles.content_fixed : Styles.content}>
-                <div className={Styles.bar} style={style} id={CONSTS.ids.appSideBar}>
-                    <TabSideBar isFixed={isFixed} />
-                </div>
-            </div>
+  return (
+    <div className={Styles.bar_wrapper}>
+      <SideBarIcon className={Styles.icon} onClick={() => handleToggle()} />
+      <div
+        style={wrapperStyle}
+        ref={wrapperRef}
+        className={isFixed ? Styles.content_fixed : Styles.content}
+      >
+        <div className={Styles.bar} style={style} id={CONSTS.ids.appSideBar}>
+          <TabSideBar isFixed={isFixed} />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default ControllerSideBar
+export default ControllerSideBar;
