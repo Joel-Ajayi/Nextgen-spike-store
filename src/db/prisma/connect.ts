c1de00a747b7f057af49f113b7232fb33feff1c4
Joel-Ajayi <ajayiayotunde13@gmail.com>
2024-11-20 12:27:07 +0100

    Resolved issue with conflicting mongo db packages

--- src/db/prisma/connect.ts
+++ src/db/prisma/connect.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 import prismaRandom from "prisma-extension-random";
-
 // Prevent multiple instances of Prisma Client in development
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
